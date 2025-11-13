7e6a9a60b23d15ce8aae5affdd6fe69c7d918cf3
leonardo <leonardolfdev@gmail.com>
2025-11-13 20:30:15 -0300

    fix: atualizar versão da aplicação para 0.0.4

--- src/shared/utils/appInfo.ts
+++ src/shared/utils/appInfo.ts
@@ -1,4 +1,4 @@
 // Informações da aplicação
 export const APP_NAME = "Lista de Tarefas";
-export const APP_VERSION = "1.0.0";
-export const BUILD_DATE = "16/09/2025"; // Data será atualizada automaticamente no build
+export const APP_VERSION = "0.0.4";
+export const BUILD_DATE = "13/11/2025"; // Data será atualizada automaticamente no build
